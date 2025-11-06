820063a66985d59df27db653cbb2f1bfd287e4aa
Imhrutik05 <hrutikchaudhari5@gmail.com>
2025-11-06 12:29:21 +0530

    Modified the temperature

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
 
-let temp = { temperature: 20, conditions: "Sunny" };
+let temp = { temperature: 25, conditions: "Sunny" };
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(temp);
